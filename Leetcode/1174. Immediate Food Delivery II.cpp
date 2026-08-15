ce1667887c9a936ff389d75526dff9d04249a298
Yogesh Rana <yogeshrana2301@gmail.com>
2026-08-15 16:43:13 +0530

    Create 1174. Immediate Food Delivery II.cpp

---
+++ Leetcode/1174. Immediate Food Delivery II.cpp
@@ -0,0 +1,21 @@
+WITH first_orders AS (
+    SELECT 
+        customer_id,
+        MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
+)
+SELECT 
+    ROUND(
+        100 * SUM(
+            CASE
+                WHEN d.order_date = d.customer_pref_delivery_date THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM Delivery AS d
+JOIN first_orders AS f
+    ON d.customer_id = f.customer_id
+    AND d.order_date = f.first_order_date;
