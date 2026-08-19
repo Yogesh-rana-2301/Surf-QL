b17ac472bdc9265ccd72c24034ce0b022ac483a5
Yogesh Rana <yogeshrana2301@gmail.com>
2026-08-20 02:52:29 +0530

    Update 1527. Patients With a Condition.cpp

--- Leetcode/1527. Patients With a Condition.cpp
+++ Leetcode/1527. Patients With a Condition.cpp
@@ -3,3 +3,11 @@ SELECT patient_id, patient_name, conditions
 FROM Patients
 WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
 ; 
+
+
+
+# Write your MySQL query statement below
+select *
+from Patients
+where instr(conditions," DIAB1") !=0 or instr(conditions,"DIAB1") =1
+order by patient_id
